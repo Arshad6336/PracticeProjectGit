d920a408b8056bd58c5218eb101fc804ac1dff70
LENOVO <LENOVO@DESKTOP-IAMFQOJ>
2024-07-23 11:32:51 +0530

    This is my first commit

---
+++ gitTesting/src/test/java/gitTesting/com/Test1.java
@@ -0,0 +1,14 @@
+package gitTesting.com;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str="hungry";
+		
+		System.out.println(str);
+
+	}
+
+}
